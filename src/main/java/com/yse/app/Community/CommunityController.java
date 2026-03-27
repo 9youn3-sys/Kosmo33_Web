81d3321ec35f025bbaa13a879b748dec8d315c8d
YoonShinEun <dbstlsdms56@daum.net>
2026-03-27 18:31:51 +0900

    dd

---
+++ src/main/java/com/yse/app/Community/CommunityController.java
@@ -0,0 +1,59 @@
+package com.yse.app.Community;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Servlet implementation class CommunityController
+ */
+@WebServlet("/comm/list")
+public class CommunityController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CommunityController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+		CommunityDAO dao = new CommunityDAO();
+		
+		
+		try {
+			ArrayList<CommunityDTO> ar = dao.list();
+			request.setAttribute("list", ar);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/comm/list.jsp");
+		view.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
